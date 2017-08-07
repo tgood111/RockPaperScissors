e01fca77487545f59ee7c76d0b4dc369b750c383
tgood111 <tgood111@live.com>
2017-08-07 08:29:31 -0500

    commented out game layout

---
+++ RockPaperScissorLizardSpock/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissorLizardSpock
+{
+    public class Game
+    { 
+    //1. display rules
+    //2. how many players
+    //3. get player names
+    //4. make a choice
+    //5. Compare choices to determine a round winner
+    //6. Give point to round winner
+    //7. Check if we have a game winner (if score == 2)
+    //8. If we don't have a game winner, rinse and repeat from step 4
+    }
+}
---
+++ RockPaperScissorLizardSpock/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissorLizardSpock
+{
+    public class Player
+    {
+        int score;
+        string name;
+        string choice;
+    }
+}
