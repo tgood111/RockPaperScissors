5a63ad1e8d3cd4769cf4bd96e4df254f728bdc77
tgood111 <tgood111@live.com>
2017-08-07 21:23:32 -0500

    added DetermineWinner Method

--- RockPaperScissorLizardSpock/Game.cs
+++ RockPaperScissorLizardSpock/Game.cs
@@ -17,7 +17,7 @@ namespace RockPaperScissorLizardSpock
         }
         public void GetNumberOfPlayers()
         {
-
+            Console.WriteLine();
         }
 
         public void ComparePlayerChoices()
@@ -38,6 +38,108 @@ namespace RockPaperScissorLizardSpock
             {
                 Console.WriteLine(playerOne.name + " won this round.");
                 playerOne.IncrementScore();
+            }
+            else if (playerOne.choice == "Rock" && playerTwo.choice == "Lizard")
+            {
+                Console.WriteLine(playerOne.name + " won this round.");
+                playerOne.IncrementScore();
+            }
+            else if (playerOne.choice == "Rock" && playerTwo.choice == "Spock")
+            {
+                Console.WriteLine(playerTwo.name + " won this round.");
+                playerTwo.IncrementScore();
+            }
+            else if (playerOne.choice == "Paper" && playerTwo.choice == "Rock")
+            {
+                Console.WriteLine(playerOne.name + " won this round.");
+                playerOne.IncrementScore();
+            }
+            else if (playerOne.choice == "Paper" && playerTwo.choice == "Scissors")
+            {
+                Console.WriteLine(playerTwo.name + " won this round.");
+                playerTwo.IncrementScore();
+            }
+            else if (playerOne.choice == "Paper" && playerTwo.choice == "Lizard")
+            {
+                Console.WriteLine(playerTwo.name + " won this round.");
+                playerTwo.IncrementScore();
+            }
+            else if (playerOne.choice == "Paper" && playerTwo.choice == "Spock")
+            {
+                Console.WriteLine(playerOne.name + " won this round.");
+                playerOne.IncrementScore();
+            }
+            else if (playerOne.choice == "Scissors" && playerTwo.choice == "Paper")
+            {
+                Console.WriteLine(playerOne.name + " won this round.");
+                playerOne.IncrementScore();
+            }
+            else if (playerOne.choice == "Scissors" && playerTwo.choice == "Rock")
+            {
+                Console.WriteLine(playerTwo.name + " won this round.");
+                playerTwo.IncrementScore();
+            }
+            else if (playerOne.choice == "Scissors" && playerTwo.choice == "Lizard")
+            {
+                Console.WriteLine(playerOne.name + " won this round.");
+                playerOne.IncrementScore();
+            }
+            else if (playerOne.choice == "Scissors" && playerTwo.choice == "Spock")
+            {
+                Console.WriteLine(playerTwo.name + " won this round.");
+                playerTwo.IncrementScore();
+            }
+            else if (playerOne.choice == "Lizard" && playerTwo.choice == "Rock")
+            {
+                Console.WriteLine(playerTwo.name + " won this round.");
+                playerTwo.IncrementScore();
+            }
+            else if (playerOne.choice == "Lizard" && playerTwo.choice == "Paper")
+            {
+                Console.WriteLine(playerOne.name + " won this round.");
+                playerOne.IncrementScore();
+            }
+            else if (playerOne.choice == "Lizard" && playerTwo.choice == "Scissors")
+            {
+                Console.WriteLine(playerTwo.name + " won this round.");
+                playerTwo.IncrementScore();
+            }
+            else if (playerOne.choice == "Lizard" && playerTwo.choice == "Spock")
+            {
+                Console.WriteLine(playerOne.name + " won this round.");
+                playerOne.IncrementScore();
+            }
+            else if (playerOne.choice == "Spock" && playerTwo.choice == "Rock")
+            {
+                Console.WriteLine(playerOne.name + " won this round.");
+                playerOne.IncrementScore();
+            }
+            else if (playerOne.choice == "Spock" && playerTwo.choice == "Paper")
+            {
+                Console.WriteLine(playerTwo.name + " won this round.");
+                playerTwo.IncrementScore();
+            }
+            else if (playerOne.choice == "Spock" && playerTwo.choice == "Scissors")
+            {
+                Console.WriteLine(playerOne.name + " won this round.");
+                playerOne.IncrementScore();
+            }
+            else if (playerOne.choice == "Spock" && playerTwo.choice == "Lizard")
+            {
+                Console.WriteLine(playerTwo.name + " won this round.");
+                playerTwo.IncrementScore();
+            }
+        }
+
+        public void DeterineGameWinner()
+        {
+                if(playerOne.roundScore == 2)
+                {
+                Console.WriteLine($"{ playerOne.name} wins the game");
+                }
+                else if(playerTwo.roundScore == 2)
+                {
+                Console.WriteLine($"{playerTwo.name} wins");
             }
         }
     //1. display rules
--- RockPaperScissorLizardSpock/Rules.cs
+++ RockPaperScissorLizardSpock/Rules.cs
@@ -14,7 +14,7 @@ namespace RockPaperScissorLizardSpock
         }
         public void DisplayRules()
         {
-            Console.WriteLine("Rock, Paper, Scissors, Lizard, Spock: Scissors cuts Paper, Paper covers Rock, Rock crushes Lizard, Lizard poisons Spock, Spock smashes Scissors, Scissors decapitates Lizard, Lizard eats Paper, Paper disproves Spock, Spock vaporizes Rock, (and as it always has) Rock crushes Scissors. Best of three to decide winner");
+            Console.WriteLine("Rock, Paper, Scissors, Lizard, Spock: Scissors cuts Paper, Paper covers Rock, Rock crushes Lizard, Lizard poisons       Spock, Spock smashes Scissors, Scissors decapitates Lizard, Lizard eats Paper, Paper disproves Spock, Spock vaporizes   Rock, Rock crushes Scissors. Best of three to decide winner");
         }
     }
 }
